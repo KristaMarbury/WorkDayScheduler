008d4d73983d36a3f70371f9fe73de2861bb8d0a
Krista Marbury <laetaiustus@gmail.com>
2021-04-29 13:54:17 -0500

    made a working button added one table row and did some pseudo coding

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
-var timeDisplayEl = $("#time-display");
+let timeDisplayEl = $("#time-display");
+let textTask1 = $("#task1").text();
 
 // handle displaying the time
 function displayTime() {
-  var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+  let rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
   timeDisplayEl.text(rightNow);
 }
 
 setInterval(displayTime, 1000);
+
+$("#hour1").click(function () {
+  alert(textTask1);
+  //save text task1 to local storage and then pull it back
+});
